b65be64d22bd815421f72517c3ececa54b0c6c96
Ömer Yücel <omeryucel94@gmail.com>
2017-01-26 22:44:54 +0200

    update notlar.php

--- notlar.php
+++ notlar.php
@@ -50,55 +50,56 @@
 	
 	</br> </br> </br>
 	
-			<table align="center" cellspacing="30" style="font-size:30px;">
+		<table align="center" cellspacing="30" style="font-size:30px;">
+		
+		<tr>
+			<td> <img src="img/icons/add_icon.png" width="80" height="50"> </img> <a href="rehber/not_ekle.php" target=""> Not Ekle </a> </td>
+			<td> <img src="img/icons/delete_icon.png" width="80" height="50"> </img> <a href="rehber/not_sil.php" target=""> Not Sil </a> </td>
+		</tr>
 			
-			<tr>
-				<td> <img src="img/icons/add_icon.png" width="80" height="50"> </img> <a href="rehber/not_ekle.php" target=""> Not Ekle </a> </td>
-				<td> <img src="img/icons/delete_icon.png" width="80" height="50"> </img> <a href="rehber/not_sil.php" target=""> Not Sil </a> </td>
-			</tr>
-			
-		</table>
+	</table>
 
 	<?php
 
 		/*
 		$dir = "rehber/$login_user/notlar";
 		$dh  = opendir($dir);
-		while (false !== ($filename = readdir($dh))) {
-		$files[] = $filename;
+		while (false !== ($filename = readdir($dh))) 
+		{
+			$files[] = $filename;
 		}
-			print(count($files));
-			print_r($files);
+		print(count($files));
+		print_r($files);
 		*/
 
-				
-		$konum  = "rehber/$login_user/notlar";
-		$files1 = scandir($konum);
-		$uzunluk = count($files1);
+		$konum="rehber/$login_user/notlar";
+		$dosyalar=scandir($konum);
+		$uzunluk=count($dosyalar);
 		
 		print("<h3 align=\"center\"> Kayıtlı Olan Notlar: </h3>");
-		print("<table align=\"center\" width=\"50%\" border=\"6\" cellspacing=\"0\" cellpadding=\"0\" background=\"img/wallpaper6.jpg\" style=\"border: 1px solid black; text-align:center;\" >");
+		print("<table align=\"center\" width=\"50%\" border=\"6\" cellspacing=\"0\" cellpadding=\"0\" background=\"img/wallpaper6.jpg\" style=\"border: 1px solid black; text-align:center; font-size:20px; \" >");
 		print("<tr>");
-		print("<th> Notlar </th>");
+		print("<th> NOT ADI </th>");
+		print("<th> NOT İÇERİĞİ </th>");
 		print("</tr>");
 		
 		for($i=2;$i<$uzunluk;$i++)
 		{
-			$dosya=fopen("rehber/$login_user/notlar/$files1[$i]",'r') or die("Dosya Açılamadı!");	
-			  
+			$dosya=fopen("rehber/$login_user/notlar/$dosyalar[$i]",'r') or die("Dosya Açılamadı!");	
+			print("<tr align=\"left\">");
+			print("<th align=\"center\">". $dosyalar[$i]."</th>");
 			while( !feof($dosya) )
 			{
-				$bilgiler=fgets($dosya);
-				print("<tr align=\"center\">");
-				print("<td>". @$bilgiler."</td>");
-				print("</tr>");
+				$notlar=fgets($dosya);	
+				if(strlen($notlar)!=0)
+					print("<td>". $notlar."</td>");	
 			}
-			
+			print("</tr>");
+			fclose($dosya);
 		}
 		print("</table>");
-		fclose($dosya);
+		
 		?>
-
 		
 </body>
 
